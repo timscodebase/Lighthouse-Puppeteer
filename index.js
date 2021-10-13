165ac4a89109924af15b572b14a5494761a46cee
Tim Smith <gonad2019@gmail.com>
2021-10-12 19:25:13 -0700

    Trying for local.

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
 
 (async () => {
-  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const url =
+    "https://local.secure-www.stage.gaptechol.com/checkout/place-order/";
+  const chrome = await chromeLauncher.launch({
+    chromeFlags: [
+      // "--headless",
+      // "--disable-mobile-emulation",
+      //"--no-sandbox",
+      "--allow-running-insecure-content",
+      "--ignore-certificate-errors",
+    ],
+    logLevel: "verbose",
+    output: "json",
+    disableDeviceEmulation: true,
+    defaultViewport: {
+      width: 1200,
+      height: 900,
+    },
+  });
   const options = {
-    logLevel: "info",
+    logLevel: "verbose",
     output: "html",
-    onlyCategories: ["performance"],
+    //onlyCategories: ["performance"],
     port: chrome.port,
   };
-  const runnerResult = await lighthouse(
-    "https://secure-www.gap.com/checkout/place-order/",
-    options
-  );
+
+  const runnerResult = await lighthouse(url, options);
 
   // `.report` is the HTML report as a string
   const reportHtml = runnerResult.report;
